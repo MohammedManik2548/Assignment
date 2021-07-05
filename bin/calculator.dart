985d16571e85957093b580d43acfc58779a2660f
MohammedManik2548 <mmanik2548@gmail.com>
2021-07-06 03:46:06 +0600

    This is my first commit

---
+++ bin/calculator.dart
@@ -0,0 +1,17 @@
+int sub(int num1, int num2) {
+
+  int num3 = num1 - num2;
+  return num3;
+}
+
+int mul(int num1, int num2) {
+
+  int num3 = num1 * num2;
+  return num3;
+}
+
+double div(double num1,double num2) {
+
+  double num3 = num1 / num2;
+  return num3;
+}
---
+++ bin/main.dart
@@ -0,0 +1,12 @@
+import 'calculator.dart';
+
+void main(List<String> arguments) {
+  int result1 = sub(10, 2);
+  print("The Subtraction is = ${result1}");
+
+  int result2 = mul(3, 5);
+  print("The Multiplication is = ${result2}");
+
+  double result3 = div(20, 5);
+  print("The Division is = ${result3}");
+}
